f925f6ed6b4960e7887ebec4623d3d43d7ae5f3d
Nora Terbocs <nora.terbocs@gmail.com>
2023-06-03 15:38:19 +0200

    Added chart to my account page

--- src/pages/myAccountPage/MyActivity.js
+++ src/pages/myAccountPage/MyActivity.js
@@ -1,21 +1,30 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Chart } from 'react-google-charts';
+
+export const options = {
+  chart: {
+    title: 'Score per quiz'
+  }
+};
 
 export const MyActivity = () => {
   const myActivity = useSelector((store) => store.user.history)
+  const performance = [['Date', 'Score']]
+  myActivity.map((item) => {
+    return performance.push([item.timestamp, item.score])
+  })
+  console.log(performance)
   return (
     <Container>
       <Typography variant="h4">My activity: </Typography>
-      {myActivity.map((item) => {
-        return (
-          <Box key={item.quizname}>
-            <Typography>{item.quizname}</Typography>
-            <Typography>{item.score}</Typography>
-            <Typography>{item.timestamp}</Typography>
-          </Box>
-        )
-      })}
+      <Chart
+        chartType="Line"
+        width="100%"
+        height="400px"
+        data={performance}
+        options={options} />
     </Container>
   )
 }
