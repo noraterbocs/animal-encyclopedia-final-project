abe2a7b5c1f0582476a5c8006a2b7fe51b408105
Nora Terbocs <nora.terbocs@gmail.com>
2023-06-05 21:13:56 +0200

    Added leaderboard without styling

--- src/reducers/leaderboard.js
+++ src/reducers/leaderboard.js
@@ -1,10 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { API_URL } from 'utils/urls';
+import { user } from './user';
+import { loading } from './loading';
 
 export const leaderboard = createSlice({
   name: 'leaderboard',
   initialState: {
-    items: [],
-    leaderboard: '',
+    leaderboard: [],
     error: null
   },
   reducers: {
@@ -13,6 +15,34 @@ export const leaderboard = createSlice({
     },
     setLeaderboard: (store, action) => {
       store.leaderboard = action.payload
+      console.log(action.payload)
     }
   }
 });
+
+// GET all users data
+export const getUsers = () => {
+  return (dispatch, getState) => {
+    const { accessToken } = getState().user;
+    const options = {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: accessToken
+      }
+    }
+    fetch(API_URL('users'), options)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          dispatch(leaderboard.actions.setLeaderboard(data.response));
+          dispatch(user.actions.setError(null))
+        } else {
+          dispatch(user.actions.setError(data.response))
+          dispatch(loading.actions.setLoading(false))
+        }
+      })
+      .finally(() => setTimeout(() => dispatch(loading.actions.setLoading(false)), 5000))
+  };
+};
