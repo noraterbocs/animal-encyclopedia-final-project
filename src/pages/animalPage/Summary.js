0c47ad19ae127fbb992a82c22f04410c1cff02d3
Nora Terbocs <nora.terbocs@gmail.com>
2023-06-12 14:48:28 +0200

    Fixed badges

--- src/pages/animalPage/Summary.js
+++ src/pages/animalPage/Summary.js
@@ -12,6 +12,7 @@ import Confetti from 'react-confetti'
 // import { animalArticles } from 'reducers/articles';
 import { fetchAnimalArticles } from 'reducers/articles';
 import Stack from '@mui/material/Stack';
+import { updateBadges } from 'reducers/user';
 import { Chatbot } from './ChatBot';
 import ChatbotAvatar from '../../assets/chatbot/195.jpg';
 import SummaryPicture from '../../assets/summary/summarypic.jpg';
@@ -42,10 +43,23 @@ const ChatbotAvatarStyle = {
   height: '60px'
 }
 
+const userBadges = [
+  { id: '1', title: 'explorer', path: '/images/badges/Explorer.png', description: 'Congratulations! You have earned the Explorer badge! As an explorer, you have taken your first steps into the exciting world of animals. You have shown curiosity and a keen interest in learning about different species. Keep exploring and discovering fascinating facts about animals from all around the world.' },
+  { id: '2', title: 'apprentice', path: '/images/badges/Apprentice.png', description: 'Well done on achieving the Apprentice badge! You are now progressing beyond the beginner stage and showing dedication to expanding your animal knowledge. As an apprentice, you are developing a deeper understanding of various species and their unique characteristics. Continue to explore, ask questions, and grow your expertise in the animal kingdom.' },
+  { id: '3', title: 'speciesSleuth', path: '/images/badges/SpeciesSleuth.png', description: 'Fantastic work! You have earned the Species Sleuth badge! As a species sleuth, you have proven yourself to be a skilled investigator of the animal world. You have developed an eye for detail and can identify different species based on their distinctive features. Your knowledge and observation skills are becoming more advanced, enabling you to unravel the secrets of the animal kingdom.' },
+  { id: '4', title: 'juniorZoologist', path: '/images/badges/JuniorZoologist.png', description: 'Congratulations on reaching the Junior Zoologist badge! You have now become a young expert in the field of zoology. Your dedication and hard work have paid off, and you have gained a wealth of knowledge about various animal groups, their habitats, and behaviors. Your passion for animals shines through as you continue to explore and deepen your understanding of the incredible diversity of life on Earth.' },
+  { id: '5', title: 'seniorZoologist', path: '/images/badges/SeniorZoologist.png', description: 'Well done! You have achieved the prestigious Senior Zoologist badge! As a senior zoologist, you have become a true authority on the subject of animals. Your extensive knowledge and experience make you a valuable resource for others seeking information about the natural world. You have demonstrated a deep understanding of complex concepts and can analyze animal behavior and ecological relationships with expertise.' },
+  { id: '6', title: 'wildlifeChampion', path: '/images/badges/WildlifeChampion2.png', description: 'Congratulations! You have reached the pinnacle of animal knowledge and earned the esteemed Wildlife Champion badge! As a wildlife champion, you stand out as a true advocate for the conservation and protection of animal species. Your dedication to preserving the natural world and raising awareness about the importance of wildlife is truly inspiring. You are a role model for others,and your passion and commitment make a significant impact on the future of our planet&apos;s biodiversity.' }
+]
+
 export const Summary = () => {
   // useSelector from quiz reducer
   const answers = useSelector((store) => store.quiz.answers)
   const animalId = useSelector((store) => store.quiz.animalId)
+  // const allBadges = useSelector((store) => store.quiz.badges)
+  const highestBadgeRank = useSelector((store) => store.user.highestBadgeRank)
+  // const userBadges = useSelector((store) => store.user.badges)
+  const totalScore = useSelector((store) => store.user.totalScore)
   const correctAnswers = answers.filter((item) => item.isCorrect)
   const dispatch = useDispatch();
   const { quizOver } = useSelector((store) => store.quiz);
@@ -55,7 +69,26 @@ export const Summary = () => {
 
   useEffect(() => {
     dispatch(fetchAnimalArticles(animalId));
-  });
+    let newHighestBadgeRank = ''
+    if (totalScore > 60) {
+      newHighestBadgeRank = 'wildlifeChampion'
+    } else if (totalScore > 50) {
+      newHighestBadgeRank = 'seniorZoologist'
+    } else if (totalScore > 40) {
+      newHighestBadgeRank = 'juniorZoologist'
+    } else if (totalScore > 30) {
+      newHighestBadgeRank = 'speciesSleuth'
+    } else if (totalScore > 20) {
+      newHighestBadgeRank = 'apprentice'
+    } else {
+      newHighestBadgeRank = 'explorer'
+    }
+    const newBadge = userBadges.find((badge) => badge.title === newHighestBadgeRank)
+    if (newHighestBadgeRank !== highestBadgeRank) {
+      dispatch(updateBadges(newBadge))
+      console.log('earning a new badge:', 'newHighestBadgeRank:', newHighestBadgeRank, 'highestBadgeRank:', highestBadgeRank, 'newBadge:', newBadge)
+    }
+  }, []);
 
   const [summaryModalOpen, setSummaryModalOpen] = React.useState(false);
   const [chatbotModalOpen, setChatbotModalOpen] = React.useState(false);
--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -43,6 +43,15 @@ const animalAnswers = [
   { id: 'toucan', correctAnswerIndex: [1, 1, 0, 1, 2, 2], answerIndex: [] }
 ]
 
+const userBadges = [
+  { id: '1', title: 'explorer', path: '/images/badges/Explorer.png', description: 'Congratulations! You have earned the Explorer badge! As an explorer, you have taken your first steps into the exciting world of animals. You have shown curiosity and a keen interest in learning about different species. Keep exploring and discovering fascinating facts about animals from all around the world.' },
+  { id: '2', title: 'apprentice', path: '/images/badges/Apprentice.png', description: 'Well done on achieving the Apprentice badge! You are now progressing beyond the beginner stage and showing dedication to expanding your animal knowledge. As an apprentice, you are developing a deeper understanding of various species and their unique characteristics. Continue to explore, ask questions, and grow your expertise in the animal kingdom.' },
+  { id: '3', title: 'speciesSleuth', path: '/images/badges/SpeciesSleuth.png', description: 'Fantastic work! You have earned the Species Sleuth badge! As a species sleuth, you have proven yourself to be a skilled investigator of the animal world. You have developed an eye for detail and can identify different species based on their distinctive features. Your knowledge and observation skills are becoming more advanced, enabling you to unravel the secrets of the animal kingdom.' },
+  { id: '4', title: 'juniorZoologist', path: '/images/badges/JuniorZoologist.png', description: 'Congratulations on reaching the Junior Zoologist badge! You have now become a young expert in the field of zoology. Your dedication and hard work have paid off, and you have gained a wealth of knowledge about various animal groups, their habitats, and behaviors. Your passion for animals shines through as you continue to explore and deepen your understanding of the incredible diversity of life on Earth.' },
+  { id: '5', title: 'seniorZoologist', path: '/images/badges/SeniorZoologist.png', description: 'Well done! You have achieved the prestigious Senior Zoologist badge! As a senior zoologist, you have become a true authority on the subject of animals. Your extensive knowledge and experience make you a valuable resource for others seeking information about the natural world. You have demonstrated a deep understanding of complex concepts and can analyze animal behavior and ecological relationships with expertise.' },
+  { id: '6', title: 'wildlifeChampion', path: '/images/badges/WildlifeChampion2.png', description: 'Congratulations! You have reached the pinnacle of animal knowledge and earned the esteemed Wildlife Champion badge! As a wildlife champion, you stand out as a true advocate for the conservation and protection of animal species. Your dedication to preserving the natural world and raising awareness about the importance of wildlife is truly inspiring. You are a role model for others,and your passion and commitment make a significant impact on the future of our planet&apos;s biodiversity.' }
+]
+
 const initialState = {
   questions,
   animalAnswers,
@@ -54,8 +63,8 @@ const initialState = {
   correctAnswerIndicator: false,
   currentScore: 0,
   animalId: null,
-  history: []
-  // totalScore: 0
+  history: [],
+  badges: userBadges
 }
 export const quiz = createSlice({
   name: 'quiz',
@@ -69,31 +78,6 @@ export const quiz = createSlice({
       // identify current animal
       const selectedAnimal = state.animalAnswers.find((a) => a.id === animalId)
 
-      // let newAnswer = question.options[animalAnswers];
-      // state.correctAnswerIndicator = true
-      // state.disabledButtons = true
-
-      // if (newAnswer === undefined || newAnswer === null) {
-      //   newAnswer = 'Nothing selected';
-      // }
-
-      // if (!question) {
-      //   throw new Error('Could not find question!
-      // Check to make sure you are passing the question id correctly.')
-      // }
-
-      // if (question.correctAnswerIndex) {
-      //   state.btnColor = '#56ab2f'
-      // }
-      // if (question.correctAnswerIndex === answerIndex) {
-      //   console.log('correct index', question.correctAnswerIndex, 'selectedIndex', answerIndex)
-      //   state.btnColor = '#56ab2f'
-      // } else {
-      //   console.log('correct index',
-      // question.correctAnswerIndex, 'wrongselectedIndex', answerIndex)
-      //   state.btnColor = '#FF416C';
-      // }
-
       state.answers.push({
         questionId,
         selectedAnswerIndex: answerIndex,
@@ -106,13 +90,6 @@ export const quiz = createSlice({
         state.currentScore += 1
         console.log(state.currentScore)
       }
-      // const historyEntry = {
-      //   quiz: animalId,
-      //   score,
-      //   timestamp: new Date().toISOString()
-      // };
-      // state.history.push(historyEntry);
-      // state.totalScore += score;
       console.log('state.currentScore:', state.currentScore, 'corret answerindex:', selectedAnimal.correctAnswerIndex[questionId], 'answerindex:', answerIndex, 'state.answers:', state.answers)
     },
 
@@ -130,16 +107,16 @@ export const quiz = createSlice({
         state.currentQuestionIndex += 1
       }
     },
-    goToPreviousQuestion: (state) => {
-      state.disabledButtons = false;
-      state.btnColor = ''
-      state.correctAnswerIndicator = false
-      if (state.currentQuestionIndex === 0) {
-        state.currentQuestionIndex = state.questions.length - 1;
-      } else {
-        state.currentQuestionIndex -= 1
-      }
-    },
+    // goToPreviousQuestion: (state) => {
+    //   state.disabledButtons = false;
+    //   state.btnColor = ''
+    //   state.correctAnswerIndicator = false
+    //   if (state.currentQuestionIndex === 0) {
+    //     state.currentQuestionIndex = state.questions.length - 1;
+    //   } else {
+    //     state.currentQuestionIndex -= 1
+    //   }
+    // },
 
     startQuiz: (state) => {
       if (state.quizOver === null) {
