5e5adb64baa1117f49356098a115428a35cde45b
Nora Terbocs <nora.terbocs@gmail.com>
2023-06-14 13:41:57 +0200

    Changed positioning for map, added different image sizef for the breakpoints for animals

--- src/pages/homePage/BackgroundMap.js
+++ src/pages/homePage/BackgroundMap.js
@@ -2,5 +2,5 @@ import React from 'react';
 import BackgroundSVG from '../../assets/background/plainmapwithoutsea.svg'
 
 export const BackgroundMap = () => {
-  return <img style={{ bgcolor: '#4facebfc', overFlow: 'scroll', width: '800px', zIndex: -1, height: '100%' }} src={BackgroundSVG} alt="Map" />
+  return <img style={{ bgcolor: '#4facebfc', overFlow: 'scroll', width: '100%', zIndex: -1, height: '100%' }} src={BackgroundSVG} alt="Map" />
 };
