c1cb0cf9aa55f669fcb11c7c9801f9df1c878674
Nora Terbocs <nora.terbocs@gmail.com>
2023-06-15 12:04:19 +0200

    Removed Footer from login and animal page

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react';
+/* eslint-disable react/jsx-closing-tag-location */
+import React, { useEffect, useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { useMediaQuery } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 
 const Copyright = () => {
   return (
@@ -18,40 +20,55 @@ const Copyright = () => {
 
 const StickyFooter = () => {
   const isMobile = useMediaQuery('(max-width:600px)');
+  const location = useLocation();
+  // Excluding pages to show Header:
+  const [display, setDisplay] = useState(true)
+  useEffect(() => {
+    if (location.pathname.includes('animal') || location.pathname.includes('login')) {
+      setDisplay(false)
+    } else {
+      setDisplay(true)
+    }
+  }, [location])
   return (
     <Box
       sx={{
         display: 'flex',
         flexDirection: 'column'
       }}>
-      <CssBaseline />
-      <Box
-        component="footer"
-        sx={{
-          width: '100vw',
-          position: isMobile ? 'relative' : 'fixed',
-          bottom: 0,
-          padding: '1em',
-          backgroundColor: '#84a199',
-          display: 'flex',
-          alignItems: 'center'
-        }}>
-        <Container
-          maxWidth="lg"
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            maxWidth: '100% !important'
-          }}>
-          <Typography variant="body1" sx={{ color: '#04211F', textAlign: 'center', flex: 1 }}>
+      {display
+        ? <>
+          <CssBaseline />
+          <Box
+            component="footer"
+            sx={{
+              width: '100vw',
+              position: isMobile ? 'relative' : 'fixed',
+              bottom: 0,
+              padding: '1em',
+              backgroundColor: '#84a199',
+              display: 'flex',
+              alignItems: 'center'
+            }}>
+            <Container
+              maxWidth="lg"
+              sx={{
+                display: 'flex',
+                flexDirection: isMobile ? 'column' : 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                maxWidth: '100% !important'
+              }}>
+              <Typography variant="body1" sx={{ color: '#04211F', textAlign: 'center', flex: 1 }}>
               Built by Nora Terbocs and Jennifer Feenstra-Arengård
-          </Typography>
-          <Box sx={{ flexShrink: 0, pr: 2 }}>
-            <Copyright />
+              </Typography>
+              <Box sx={{ flexShrink: 0, pr: 2 }}>
+                <Copyright />
+              </Box>
+            </Container>
           </Box>
-        </Container>
-      </Box>
+        </>
+        : ''}
     </Box>
   );
 };
